3b2e6c5516e25ba9648740cedce3d441ba500bf0
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2022-10-03 12:13:10 +0100

    pratices about data tnterpolation and components

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponentComponent implements OnInit {
 
+  name="manilson";
+  age=19;
+  job="Software Developer";
+  company="Cinapse SA";
+  techs=["laravel","bootstrap","css","html","learning angular"];
+  
+  car={
+    name:'porshe',
+    year:2019,
+  };
+
   constructor() { }
 
   ngOnInit(): void {
