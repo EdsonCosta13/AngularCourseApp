57f1ddb3b68dd1b4783261b3931936e51aa09a61
edsonpaulojoaodacosta@gmail.com <edsonpaulojoaodacosta@gmail.com>
2022-10-03 22:56:29 +0100

    doing exercises about components and expressions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    FooterComponentComponent,
+    ParentDataComponent
   ],
   imports: [
     BrowserModule,
